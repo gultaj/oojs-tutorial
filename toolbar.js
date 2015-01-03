3a7ae48ec20f9bd3ff379da4b42ece169459d3cc
gultaj_home <yaschick.mail@gmail.com>
2015-01-04 02:00:19 +0300

    add html, css files. add items

--- toolbar.js
+++ toolbar.js
@@ -1,35 +1,60 @@
 var oojs = (function(oojs) {
-	var items = {};
 
-	Object.defineProperties(items, {
-		el: {
-			value: el
-		},
-		enabled: {
-			set: function(value) {
-				if (value) {
-					this.el.classList.remove("disable");
-				} else {
-					this.el.classList.add("disable");
+	var createToolbarItems = function(itemElements) {
+		var items = [];
+
+		[].forEach.call(itemElements, function(element, index) {
+			var item = {
+				toggleActiveState: function() {
+					this.activated = !this.activated;
 				}
-			},
-			get: function() {
-				return !this.el.classList.contains("disable");
-			}
-		},
-		activated: {
-			set: function(value) {
-				if (value) {
-					this.el.classList.add("active");
-				} else {
-					this.el.classList.remove("active");
+			};
+
+			Object.defineProperties(item, {
+				el: {
+					value: element,
+					enumerable: true
+				},
+				enabled: {
+					enumerable: true,
+					set: function(value) {
+						if (value) {
+							this.el.classList.remove("disabled");
+						} else {
+							this.el.classList.add("disabled");
+						}
+					},
+					get: function() {
+						return !this.el.classList.contains("disabled");
+					}
+				},
+				activated: {
+					set: function(value) {
+						if (value) {
+							this.el.classList.add("active");
+						} else {
+							this.el.classList.remove("active");
+						}
+					},
+					get: function() {
+						return this.el.classList.contains("active");
+					}
 				}
-			},
-			get: function() {
-				return this.el.classList.contains("active");
-			}
-		}
-	});
+			});
+			items.push(item);
+		});
+
+		return items;
+	};
+
+	oojs.createToolbar = function(elementId) {
+		var element = document.getElementById(elementId);
+		var items = document.querySelectorAll(".toolbar-item");
+
+		return {
+			items: createToolbarItems(items)
+		};
+	};
 	
-
-})(oojs);
+	return oojs;
+})(oojs || {});
