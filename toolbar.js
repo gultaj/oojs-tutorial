1c74daa87b5d4c77004801661b1b5f7883863bee
gultaj_home <yaschick.mail@gmail.com>
2015-01-08 12:32:26 +0300

    add constructor functions
    add constructor functions for toolbar and toolbaritem

--- toolbar.js
+++ toolbar.js
@@ -1,56 +1,108 @@
 var oojs = (function(oojs) {
 
-	var createToolbarItem = function(itemElement) {
-		var item = {
-			toggleActiveState: function() {
-				this.activated = !this.activated;
-			}
-		};
-
-		Object.defineProperties(item, {
-			el: {
-				value: itemElement,
-				enumerable: true
-			},
-			enabled: {
-				enumerable: true,
-				set: function(value) {
-					if (value) {
-						this.el.classList.remove("disabled");
-					} else {
-						this.el.classList.add("disabled");
-					}
-				},
-				get: function() {
-					return !this.el.classList.contains("disabled");
-				}
-			},
-			activated: {
-				set: function(value) {
-					if (value) {
-						this.el.classList.add("active");
-					} else {
-						this.el.classList.remove("active");
-					}
-				},
-				get: function() {
-					return this.el.classList.contains("active");
-				}
+	var ToolbarItem = function(toolbarElement) {
+		Object.defineProperties(this, {
+			__el: {
+				value: toolbarElement
 			}
 		});
-		return item;
 	};
+	Object.defineProperties(ToolbarItem.prototype, {
+		toggleActiveState: {
+			value: function() {
+				this.activated = !this.activated;
+			},
+			enumerable: true
+		},
+		enabled: {
+			set: function(value) {
+				if (value) {
+					this.__el.classList.remove("disabled");
+				} else {
+					this.__el.classList.add("disabled");
+				}
+			},
+			get: function() {
+				return !this.__el.classList.contains("disabled");
+			},
+			enumerable: true
+		},
+		activated: {
+			set: function(value) {
+				if (value) {
+					this.__el.classList.add("active");
+				} else {
+					this.__el.classList.remove("active");
+				}
+			},
+			get: function() {
+				return this.__el.classList.contains("active");
+			},
+			enumerable: true
+		}
+	});
 
 	var createToolbarItems = function(itemElements) {
 		var items = [];
 
 		[].forEach.call(itemElements, function(element, index) {
-			var item = createToolbarItem(element);
+			var item = new ToolbarItem(element);
 			items.push(item);
 		});
 
 		return items;
 	};
+
+	var Toolbar = function(toolbarElement) {
+		var items = toolbarElement.querySelectorAll('.toolbar-item');
+
+		Object.defineProperties(this, {
+			__el: {
+				value: toolbarElement
+			},
+			items: {
+				value: createToolbarItems(items),
+				enumerable: true
+			}
+		});
+	};
+	Object.defineProperties(Toolbar.prototype, {
+		add: {
+			value: function(options) {
+				var span = document.createElement('span');
+				span.className = 'toolbar-item';
+
+				this.__el.appendChild(span);
+
+				var item = new ToolbarItem(span);
+				this.items.push(item);
+			},
+			enumerable: true
+		},
+		remove: {
+			value: function(index) {
+				if (index > this.items.length || index < 0) {
+					throw new Error('Index is out of range');
+				}
+
+				var item = this.items[index];
+
+				this.__el.removeChild(item.__el);
+				this.items.splice(index, 1);
+
+				item = null;
+			},
+			enumerable: true
+
+		},
+		appendTo: {
+			value: function(parentElement) {
+				parentElement.appendChild(this.__el);
+			},
+			enumerable: true
+		}
+	});
+
 	oojs.createToolbar = function(elementId) {
 		var element = document.getElementById(elementId);
 
@@ -60,46 +112,8 @@ var oojs = (function(oojs) {
 			element.className = 'toolbar';
 		}
 
-		var items = element.querySelectorAll('.toolbar-item');
-
-		var toolbar = {
-			add: function(options) {
-				var span = document.createElement('span');
-				span.className = 'toolbar-item';
-
-				this.el.appendChild(span);
-
-				var item = createToolbarItem(span);
-				this.items.push(item);
-			},
-			remove: function(index) {
-				if (index > this.items.length || index < 0) {
-					throw new Error('Index is out of range');
-				}
-
-				var item = this.items[index];
-
-				this.el.removeChild(item.el);
-				this.items.splice(index, 1);
-
-				item = null;
-
-			},
-			appendTo: function(parentElement) {
-				parentElement.appendChild(this.el);
-			}
-		};
-
-		Object.defineProperties(toolbar, {
-			el: {
-				value: element
-			},
-			items: {
-				value: createToolbarItems(items),
-				enumerable: true
-			}
-		});
-
+		var toolbar = new Toolbar(element)
+		
 		return toolbar;
 	};
 	
